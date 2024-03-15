99d675fbe1bcfde219a8011f648701dad2a2bc7f
Malick <malick1ndiaye0@gmail.com>
2024-03-15 14:11:49 +0100

    Navbar - #PORTFOLIO-7 state done

---
+++ assets/js/navbar.js
@@ -0,0 +1,5 @@
+﻿function toggleMenu() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+    menuToggle.setAttribute('aria-expanded', !expanded);
+}

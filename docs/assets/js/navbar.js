ccad3ab21f012ecb6624ee0eb2c22a53f615fba2
Malick NDIAYE <malick1ndiaye0@gmail.com>
2024-03-16 23:35:16 +0100

    Restore actions

--- docs/assets/js/navbar.js
+++
@@ -1,5 +0,0 @@
-﻿function toggleMenu() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
-    menuToggle.setAttribute('aria-expanded', !expanded);
-}
